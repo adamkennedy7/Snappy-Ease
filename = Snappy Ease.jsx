60fa0fe2c6a30cbf21eed85698052f3aac69c1c6
adamkennedy7 <71677401+adamkennedy7@users.noreply.github.com>
2023-09-26 00:25:32 -0700

    Version 1.0

---
+++ = Snappy Ease.jsx
@@ -0,0 +1,118 @@
+﻿/**
+ * Project: Snappy Ease for Adobe After Effects
+ * Author: Adam Eugene Kennedy in collaboration with `aEIOU`
+ * License: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+// Version 1.0
+
+app.beginUndoGroup("Snappy Ease");
+
+// cOUNT tHE tOTAL nUMBER oF sUCCESSFUL kEYFRAME mODIFICATIONS
+var successCount = 0;
+
+// cURRENT cOMPOSITION
+var comp = app.project.activeItem;
+
+// sELECTED lAYERS
+var selectedLayers = comp.selectedLayers;
+var selectedProps;
+
+for (var i = 0; i < selectedLayers.length; i++) {
+    selectedProps = selectedLayers[i].selectedProperties;
+    for (var k = 0; k < selectedProps.length; k++) {
+        var prop = selectedProps[k];
+        $.writeln("Layer: " + selectedLayers[i].name);
+        $.writeln("Property: " + prop.name + ", MatchName: " + prop.matchName);
+
+        // sKIP iF nO kEYFRAMES
+        if (prop.numKeys < 1) {
+            continue;
+        }
+
+        // iTERATE tHROUGH kEYFRAMES
+        for (var kf = 1; kf <= prop.numKeys; kf++) {
+            try {
+                var inEase = prop.keyInTemporalEase(kf);
+                var outEase = prop.keyOutTemporalEase(kf);
+                
+                // mODIFY eASE tO bE "sNAPPY"
+                for (var j = 0; j < inEase.length; j++) {
+                    inEase[j].speed = 0;
+                    inEase[j].influence = 75;
+                }
+                
+                for (var j = 0; j < outEase.length; j++) {
+                    outEase[j].speed = 0;
+                    outEase[j].influence = 50;
+                }
+                
+                // sET mODIFIED eASE
+                prop.setTemporalEaseAtKey(kf, inEase, outEase);
+                $.writeln("Successfully set the Snappy Ease at keyframe " + kf);
+                successCount++;
+            } catch (e) {
+                $.writeln("Failed to process snappy snap: " + e.toString());
+            }
+        }
+    }
+}
+
+app.endUndoGroup();
+
+// sHOW cUSTOM dIALOG bOX
+var dialog = new Window("dialog", "Snappy Ease 1.0");
+dialog.orientation = "column";
+dialog.alignChildren = ["fill", "top"];
+dialog.spacing = 10;
+dialog.margins = [100, 40, 100, 40];
+
+// gROUP fOR tEXT iNFO
+var textGroup = dialog.add("group", undefined);
+textGroup.orientation = "column";
+textGroup.alignChildren = ["fill", "top"];
+textGroup.spacing = 5;
+textGroup.margins = [0, 10, 0, 10];
+
+var text1 = textGroup.add("statictext", undefined, "Snappy Ease applied to " + successCount + " keyframes:");
+text1.graphics.font = ScriptUI.newFont("Arial", "Bold", 14);
+
+var text2 = textGroup.add("statictext", undefined, "• Incoming Velocity Influence set to 75%");
+text2.graphics.font = ScriptUI.newFont("Arial", "Regular", 12);
+
+var text3 = textGroup.add("statictext", undefined, "• Outgoing Velocity Influence set to 50%");
+text3.graphics.font = ScriptUI.newFont("Arial", "Regular", 12);
+
+// gROUP fOR bUTTON
+var buttonGroup = dialog.add("group", undefined);
+buttonGroup.orientation = "row";
+buttonGroup.alignChildren = ["center", "center"];
+buttonGroup.spacing = 10;
+buttonGroup.margins = [0, 20, 0, 0];
+
+var closeButton = buttonGroup.add("button", undefined, "OK");
+closeButton.graphics.font = ScriptUI.newFont("Arial", "Bold", 16);
+closeButton.onClick = function () {
+    dialog.close();
+};
+
+dialog.defaultElement = closeButton; // sET cLOSE bUTTON aS dEFAULT eLEMENT
+
+dialog.show();
+
+
+// "sNAPPY eASE: wHERE aRT mEETS cODE, mOTION gETS sNAPPY, aND eVERY kEYFRAME iS a sTAR! 🌟"
---
+++ README.md
@@ -0,0 +1,55 @@
+# Snappy Ease for Adobe After Effects
+# Version 1.0
+
+## Description
+A script to effortlessly apply a "Snappy" easing to your keyframes in Adobe After Effects.
+
+## Authors
+- [Adam Eugene Kennedy](github.com/adamkennedy7/after_effects_snappy_ease)
+- aEIOU
+
+## License
+This project is licensed under the MIT License. See the [LICENSE.md](LICENSE.md) file for details.
+
+## Prerequisites
+- Adobe After Effects installed
+
+## Installation & use
+
+### Windows
+We're going to copy the `.jsx` file into `C:\Program Files\Adobe\Adobe After Effects 2023\Support Files\Scripts`
+
+1. Open File Explorer and navigate to `C:\Program Files\Adobe`.
+2. Locate the version of After Effects you usually work with and open its folder.
+3. Inside, locate the `Support Files` folder and then find the `Scripts` folder. 
+4. Copy the `SnappyEase.jsx` file into this `Scripts` folder.
+5. Upon restarting or opening After Effects, you will find the script under `File > Scripts`
+
+*Select keyframes in an active composition and run the script. You will see a confirmation dialog stating the results!*
+
+(In easing velocity set to 75%. Out easing velocity set to 50%.)
+
+### Mac
+1. Open Finder and navigate to `/Applications/Adobe After Effects <version>`.
+2. Open the `Scripts` folder located within.
+3. Copy the `SnappyEase.jsx` file into this `Scripts` folder.
+
+The script will then appear in the `File > Scripts` menu.
+
+## How to Run
+
+1. Select some keyframes in an open composition
+2. Go to `File > Scripts` and run `Snappy Ease v1.jsx`.
+3. Execute the script.
+
+The script will automatically modify the selected keyframes, applying a "Snappy" ease to make your animations more engaging.
+
+## Features
+
+- Automated ease setting for multiple keyframes.
+- Custom dialog box showing the number of modified keyframes.
+- Simple and efficient: Select, Run, Done!
+
+## To Do
+
+[ ] TODO: Add `Window` menu shortcut for adding panel into After Effects, for more seamless integration into AE.
